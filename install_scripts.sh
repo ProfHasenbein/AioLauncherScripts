3e97d4c610f6be5c7c625605da3eeb617ebf60dc
Thilo Wejmelka <petrawej@gmail.com>
2025-03-29 23:31:59 +0100

    initial commit

--- install_scripts.sh
+++ install_scripts.sh
@@ -4,11 +4,7 @@ source ./env
 
 ./rm-scripts.sh
 
-# Verzeichnis auf deinem Android-Gerät (nicht auf deinem PC)
 ANDROID_SCRIPTS_DIR="/data/data/com.termux/files/home/AioLauncherScripts"
-
-# Ordner auf deinem PC, der die Skripte enthält
 SCRIPTS_DIR="/home/nibblus/Documents/AioLauncherScripts"
 
-# Übertrage die Skripte auf das Android-Gerät
 adb push $SCRIPTS_DIR/*.lua $ANDROID_SCRIPTS_DIR
